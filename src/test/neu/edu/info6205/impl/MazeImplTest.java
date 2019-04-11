ccc6917639a1e7fb4c07c0ceb3bada4286de3e7f
Tse-Hsi Lin <lin.tse@husky.neu.edu>
2019-04-10 23:36:14 -0400

    GA, Maze, Robot impl, and Maze test

--- src/test/neu/edu/info6205/impl/MazeImplTest.java
+++ src/test/neu/edu/info6205/impl/MazeImplTest.java
@@ -1,13 +1,41 @@
 package neu.edu.info6205.impl;
 
 import org.junit.jupiter.api.Test;
+import neu.edu.info6205.repository.Maze;
+import neu.edu.info6205.impl.MazeImpl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MazeImplTest {
 
+    private Maze genMaze() {
+        Maze m = new MazeImpl(new int[][] {
+                { 0, 0, 0, 0, 1, 0, 1, 3, 2 },
+                { 1, 0, 1, 1, 1, 0, 1, 3, 1 },
+                { 1, 0, 0, 1, 3, 3, 3, 3, 1 },
+                { 3, 3, 3, 1, 3, 1, 1, 0, 1 },
+                { 3, 1, 3, 3, 3, 1, 1, 0, 0 },
+                { 3, 3, 1, 1, 1, 1, 0, 1, 1 },
+                { 1, 3, 0, 1, 3, 3, 3, 3, 3 },
+                { 0, 3, 1, 1, 3, 1, 0, 1, 3 },
+                { 1, 3, 3, 3, 3, 1, 1, 1, 4 } });
+
+        return m;
+    }
+
     @Test
     void getStartPosition() {
+        Maze maze = genMaze();
+        assertEquals(8, maze.getStartPosition()[0]);
+        assertEquals(0, maze.getStartPosition()[1]);
+    }
+
+    @Test
+    void testWay() {
+        Maze maze = genMaze();
+        assertEquals(true, maze.isWall(0, 6));
+        assertEquals(8, maze.getMaxX());
+        assertEquals(8, maze.getMaxY());
     }
 
     @Test
@@ -16,14 +44,23 @@ class MazeImplTest {
 
     @Test
     void isWall() {
+        Maze maze = genMaze();
+        assertEquals(true, maze.isWall(0, 6));
+        assertEquals(true, maze.isWall(0, 1));
+        assertEquals(false, maze.isWall(0, 0));
+        assertEquals(false, maze.isWall(1, 1));
     }
 
     @Test
     void getMaxX() {
+        Maze maze = genMaze();
+        assertEquals(8, maze.getMaxX());
     }
 
     @Test
     void getMaxY() {
+        Maze maze = genMaze();
+        assertEquals(8, maze.getMaxY());
     }
 
     @Test
