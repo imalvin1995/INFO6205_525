a78a7190411cc5188ea35758c14c09348e002d41
Yu-Chiao Huang <huang.yuc@husky.neu.edu>
2019-04-16 16:30:25 -0400

    update Individual test and create robot test

--- src/test/java/neu/edu/info6205/impl/IndividualImplTest.java
+++ src/test/java/neu/edu/info6205/impl/IndividualImplTest.java
@@ -3,6 +3,9 @@ package neu.edu.info6205.impl;
 import neu.edu.info6205.repository.Individual;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IndividualImplTest {
@@ -30,14 +33,40 @@ class IndividualImplTest {
     @Test
     void getGene() {
         Individual i = new IndividualImpl(123);
-
+        i.setGene(100,1);
+        int j =i.getGene(100);
+        assertEquals(1,j);
     }
 
     @Test
     void setFitness() {
+        Individual i = new IndividualImpl(123);
+        i.setFitness(10.0);
+        assertEquals(10.0,i.getFitness());
     }
 
     @Test
     void getFitness() {
+        Individual i = new IndividualImpl(123);
+        i.setFitness(100.0);
+        double j = i.getFitness();
+        assertEquals(100.0,j);
+    }
+
+    @Test
+    void setRoute() {
+        List<int[]> route = Arrays.asList(new int [] {1,2,3});
+        Individual i = new IndividualImpl(123);
+        i.setRoute(route);
+        assertEquals(route, i.getRoute());
+    }
+
+    @Test
+    void getRoute() {
+        List<int[]> route = Arrays.asList(new int [] {1,2,3});
+        Individual i = new IndividualImpl(123);
+        i.setRoute(route);
+        List<int[]> j = i.getRoute();
+        assertEquals(route, j);
     }
 }
---
+++ src/test/java/neu/edu/info6205/impl/RobotImplTest.java
@@ -0,0 +1,32 @@
+package neu.edu.info6205.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RobotImplTest {
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void makeNextAction() {
+    }
+
+    @Test
+    void getNextAction() {
+    }
+
+    @Test
+    void getSensorValue() {
+    }
+
+    @Test
+    void getPosition() {
+    }
+
+    @Test
+    void getRoute() {
+    }
+}
