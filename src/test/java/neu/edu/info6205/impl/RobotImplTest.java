201324e5a038c5f937cc7466ca3053ba1973f269
Yu-Chiao Huang <huang.yuc@husky.neu.edu>
2019-04-19 13:58:26 -0400

    add robot test

--- src/test/java/neu/edu/info6205/impl/RobotImplTest.java
+++ src/test/java/neu/edu/info6205/impl/RobotImplTest.java
@@ -1,32 +1,25 @@
 package neu.edu.info6205.impl;
 
+import neu.edu.info6205.helper.MazeGen;
+import neu.edu.info6205.repository.Robot;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RobotImplTest {
+    private static final int[] move = new int[] {1,0,0,1,1,0,1,0,1,1,0,0,0,1};
+
 
     @Test
-    void run() {
+    void startPosition() {
+        Robot r = new RobotImpl(move, MazeGen.readMaze("Maze2"),100);
+        assertEquals(4, r.getPosition()[0]);
+        assertEquals(0, r.getPosition()[1]);
     }
 
     @Test
-    void makeNextAction() {
-    }
-
-    @Test
-    void getNextAction() {
-    }
-
-    @Test
-    void getSensorValue() {
-    }
-
-    @Test
-    void getPosition() {
-    }
-
-    @Test
-    void getRoute() {
+    void heading() {
+        Robot r = new RobotImpl(move, MazeGen.readMaze("Maze4"),100);
+        assertEquals(47,r.getSensorValue());
     }
 }
